8e3d17135534bbcfbbc1dbe2360fe36a2d79ed00
diam78 <mehtanideepak@gmail.com>
2015-06-23 08:53:20 +0000

    updated

--- app/app.js
+++ app/app.js
@@ -2,11 +2,39 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
+  'ui.router',
+  'ngMaterial',
   'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+])
+
+.config(function($stateProvider, $urlRouterProvider, $mdThemingProvider, $mdIconProvider) {
+  
+	$urlRouterProvider.otherwise('/view1');
+    
+  $stateProvider
+      
+    .state('view1', {
+        url: '/view1',
+        templateUrl: 'views/view1.html'
+    })
+    
+    
+    .state('view2', {
+        url: '/view2',
+        templateUrl: 'views/view2.html'
+    });
+  
+  
+  $mdIconProvider
+    .defaultIconSet("./assets/svg/avatars.svg", 128)
+    .icon("menu"       , "./assets/svg/menu.svg"        , 24)
+    .icon("share"      , "./assets/svg/share.svg"       , 24)
+    .icon("google_plus", "./assets/svg/google_plus.svg" , 512)
+    .icon("hangouts"   , "./assets/svg/hangouts.svg"    , 512)
+    .icon("twitter"    , "./assets/svg/twitter.svg"     , 512)
+    .icon("phone"      , "./assets/svg/phone.svg"       , 512);
+
+  $mdThemingProvider.theme('default')
+    .primaryPalette('brown')
+    .accentPalette('red');
+});
